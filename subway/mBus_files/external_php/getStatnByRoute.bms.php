07673cfe647d461da8e8b442115fa9c1cdfa27cd
박성원 <skfhddlg@naver.com>
2023-06-20 14:28:39 +0900

    백업용

---
+++ subway/mBus_files/external_php/getStatnByRoute.bms.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Max-Age: 86400');
+header('Access-Control-Allow-Headers: x-requested-with');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
+header('Content-Type: application/x-json;charset=EUC-KR');
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://m.bus.go.kr/mBus/subway/getStatnByRoute.bms" . "?subwayId=" . $_GET["subwayId"]);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+$result = curl_exec($ch);
+curl_close($ch);
+
+echo $result;
+?>
